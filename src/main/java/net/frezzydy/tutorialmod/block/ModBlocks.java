e4c56cec37c56928ce88b8f2c1300d8495306cb4
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-06-11 06:59:53 +0200

    [ADD] Ore Blocks
    [ADD] alexandrite_ore & alexandrite_deepslate_ore blocks
    [ADD] alexandrite_ore & alexandrite_deepslate_ore textures & models

--- src/main/java/net/frezzydy/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/frezzydy/tutorialmod/block/ModBlocks.java
@@ -2,9 +2,11 @@ package net.frezzydy.tutorialmod.block;
 
 import net.frezzydy.tutorialmod.TutorialMod;
 import net.frezzydy.tutorialmod.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,10 +23,16 @@ public class ModBlocks {
     public static final RegistryObject<Block> ALEXANDRITE_BLOCK = registryBlock("alexandrite_block",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
-
     public static final RegistryObject<Block> RAW_ALEXANDRITE_BLOCK = registryBlock("raw_alexandrite_block",
             () -> new Block(BlockBehaviour.Properties.of()
                     .strength(3f).requiresCorrectToolForDrops()));
+
+    public static final RegistryObject<Block> ALEXANDRITE_ORE = registryBlock("alexandrite_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops()));
+    public static final RegistryObject<Block> ALEXANDRITE_DEEPSLATE_ORE = registryBlock("alexandrite_deepslate_ore",
+            () -> new DropExperienceBlock(UniformInt.of(3, 6), BlockBehaviour.Properties.of()
+                    .strength(5f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
 
     private static <T extends Block> RegistryObject<T> registryBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
